2eaf637ec4de600dc411b89e842e49c06ee43597
nag <nagtraininglab@gmail.com>
2023-07-11 10:37:25 +0530

    initial commit with greet-api endpoint

---
+++ backend-service/src/test/java/com/example/GreetController.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class GreetController {
+
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    public void greetApiTest(){
+
+        client.get()
+                .uri("/api/greet")
+                .exchange()
+                .expectStatus().isOk();
+
+    }
+
+}
